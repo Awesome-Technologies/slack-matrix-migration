5525fa892520a78ecd7a80d04f5616240e7d5730
Michael Albert <michael.albert@awesome-technologies.de>
2020-07-30 19:48:23 +0200

    Format percentage output

--- migrate.py
+++ migrate.py
@@ -128,7 +128,7 @@ def update_progress(progress):
         progress = 1
         status = "Done...\r\n"
     block = int(round(barLength*progress))
-    text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status)
+    text = "\rPercent: [{0}] {1:3.2f}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status)
     sys.stdout.write(text)
     sys.stdout.flush()
 
